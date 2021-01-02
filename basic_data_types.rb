efb1f701a9ab2f2e15477a1e83ead616ad059a66
srujaan <srujans321@gmail.com>
2021-01-02 08:06:24 -0800

    Basic data types

---
+++ basic_data_types.rb
@@ -0,0 +1,20 @@
+# Addition
+# puts method prints the result to the screen
+puts(1 + 1)
+
+# Subtraction
+puts(3 - 2)
+
+# Multiplication
+puts(2 * 2)
+
+# Division
+# Quotient divison
+puts(10 / 5)
+
+# Exponent or square root of
+puts(2 ** 2)
+
+# Modulous (find the remainder of or division)
+puts(8 % 2)
+puts(23 % 3)
