d275f7040158dbc3287201dcc15c344597e3d056
srujaan <srujans321@gmail.com>
2021-01-04 09:07:17 -0800

    Practice variables

---
+++ variables.rb
@@ -0,0 +1,12 @@
+# DIFFERENT TYPES OF FILE I/0 
+var1 = "This is variable 1"
+var2 = "This is variable 2"
+puts(var1)
+puts(var2)
+
+# GET input from user
+# puts("Enter a value: ")
+# val = gets
+
+str = "Hello world"
+putc stR
