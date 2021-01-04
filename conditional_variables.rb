0681b971b4b8596ead6170128dc40e63b0502ac2
srujaan <srujans321@gmail.com>
2021-01-04 10:03:36 -0800

    Case and ternery operator

--- conditional_variables.rb
+++ conditional_variables.rb
@@ -27,3 +27,27 @@ puts(6.eql?(6)) # true
 puts(1 <=> 2)
 puts(1 <=> 1)
 puts(10 <=> 5)
+
+# CASE STATEMENTS
+#
+song = "Numb"
+song_playing = case song
+  when "Let it be" then "Beatles"
+  when "Love story" then "Taylor swift"
+  when "Numb" then "Linkin park"
+  else "Something else"
+end
+puts(song_playing) 
+
+# Unless statements
+#
+
+age = 18
+unless age < 17
+  puts "Get a job"
+end
+
+# TERNERY OPERATOR
+song = "lost in time"
+response = song == "Hello" ? "sang by Adle" :  "I don't know"
+puts response
